138301ed0e14290bd5f7cc5d7bd30f71e6f38a75
Filipe de Lima <filipin56467@gmail.com>
2021-05-19 17:14:14 -0400

    projeto iniciado, mostrando a mensagem Ola mundo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,8 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola mundo");
+            Console.ReadLine();
         }
     }
 }
