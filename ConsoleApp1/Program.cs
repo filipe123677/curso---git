f68bbd1633cc75e563bd310f45c18324a1ff32cd
Filipe de Lima <filipin56467@gmail.com>
2021-05-19 17:48:57 -0400

    acrescentada a palavra bom dia como esta

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Ola mundo");
+            Console.WriteLine("Bom dia, como esta hoje  ");
             Console.ReadLine();
         }
     }
